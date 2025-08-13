460b6f3f1d676f18e710a8db082570bacbd8e428
gayatri10121994 <gayatridange97@gmail.com>
2025-08-13 12:12:03 +0530

    Add files via upload

---
+++ cleaning.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+df = pd.read_csv('Superstore_sales_dataset.csv')
+
+df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')
+
+df['order_date'] = pd.to_datetime(df['order_date'], errors='coerce', dayfirst=False)
+df['ship_date'] = pd.to_datetime(df['ship_date'], errors='coerce', dayfirst=False)
+
+df['postal_code'].fillna(method='ffill', inplace=True)  # forward fill
+
+df.drop_duplicates(inplace=True)
+
+df.reset_index(drop=True, inplace=True)
+
+df.to_csv('superstore_sales_cleaned.csv', index=False)
+
+print("✅ Data cleaned and saved as 'superstore_sales_cleaned.csv'")
